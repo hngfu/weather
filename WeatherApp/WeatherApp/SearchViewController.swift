bdc78d78335f40837554cb07e6da58ce4b0a6a1e
hngfu <djsl1018@naver.com>
2019-08-03 22:20:19 +0900

    SearchViewController 추가

---
+++ WeatherApp/WeatherApp/SearchViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SearchViewController.swift
+//  WeatherApp
+//
+//  Created by 조재흥 on 03/08/2019.
+//  Copyright © 2019 Hngfu. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+}
