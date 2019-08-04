faa99ac5e1010d3c15e5fa098c139b78a7d69348
hngfu <djsl1018@naver.com>
2019-08-04 21:12:47 +0900

    [SearchViewController] setLocationSearchBar() 생성
    LocationSearchBar를 추가하는 코드가 모여있음

--- WeatherApp/WeatherApp/SearchViewController.swift
+++ WeatherApp/WeatherApp/SearchViewController.swift
@@ -12,6 +12,10 @@ class SearchViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setLocationSearchBar()
+    }
+    
+    private func setLocationSearchBar() {
         let searchBar = LocationSearchBar()
         searchBar.delegate = self
         self.navigationItem.titleView = searchBar
