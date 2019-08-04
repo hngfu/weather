e2b24c97d94e0d3434896a6c8d170bb14b83e906
hngfu <djsl1018@naver.com>
2019-08-04 23:03:56 +0900

    [CityManager] 추가
    도시 데이터를 관리할 모델

---
+++ WeatherApp/WeatherApp/CityManager.swift
@@ -0,0 +1,13 @@
+//
+//  CityManager.swift
+//  WeatherApp
+//
+//  Created by 조재흥 on 04/08/2019.
+//  Copyright © 2019 Hngfu. All rights reserved.
+//
+
+import Foundation
+
+struct CityManager {
+    
+}
