3a03c91f5f73c185614aa5c9bcc72eb79050db42
hngfu <djsl1018@naver.com>
2019-08-06 01:14:25 +0900

    [Main.storyboard] 섭씨, 화씨 토글버튼 추가. add 버튼 추가
    add 버튼을 touch up하면 SearchVC가 띄워져서 검색 후 지역 추가 하도록 함
    ViewController가 UITableVIewDataSource, Delegate를 채택, 준수 하도록 함

--- WeatherApp/WeatherApp/ViewController.swift
+++ WeatherApp/WeatherApp/ViewController.swift
@@ -18,7 +18,26 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        weatherTableView.dataSource = self
+        weatherTableView.delegate = self
+    }
+    
+    @IBAction func touchUpAddButton(_ sender: UIButton) {
+        sender.isSelected.toggle()
     }
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "", for: indexPath)
+        
+        return cell
+    }
+}
+
+extension ViewController: UITableViewDelegate {
+}
