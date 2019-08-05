1220803fd59d5a2f325f9d23643db059957de1e9
hngfu <djsl1018@naver.com>
2019-08-05 20:50:15 +0900

    [Main.storyboard] wetherTableView 추가
    날씨 정보를 보여줄 테이블 뷰

--- WeatherApp/WeatherApp/ViewController.swift
+++ WeatherApp/WeatherApp/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var weatherTableView: UITableView!
+    
     override var preferredStatusBarStyle: UIStatusBarStyle {
         return .lightContent
     }
