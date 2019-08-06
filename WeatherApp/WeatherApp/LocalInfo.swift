f27fc0cc75a82a5e0dfd97cdf3591bc66a055924
hngfu <djsl1018@naver.com>
2019-08-06 18:22:45 +0900

    LocalInfo 추가

---
+++ WeatherApp/WeatherApp/LocalInfo.swift
@@ -0,0 +1,14 @@
+//
+//  LocalInfo.swift
+//  WeatherApp
+//
+//  Created by 조재흥 on 06/08/2019.
+//  Copyright © 2019 Hngfu. All rights reserved.
+//
+
+import Foundation
+
+struct LocalInfo {
+    let name: String
+    let coordinate: Coordinate
+}
