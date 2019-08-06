b42024b47097009cdb646794a22bb785301821af
hngfu <djsl1018@naver.com>
2019-08-06 18:19:23 +0900

    WeatherManager 추가
    WeatherViewController의 모델역할

---
+++ WeatherApp/WeatherApp/WeatherManager.swift
@@ -0,0 +1,13 @@
+//
+//  WeatherManager.swift
+//  WeatherApp
+//
+//  Created by 조재흥 on 06/08/2019.
+//  Copyright © 2019 Hngfu. All rights reserved.
+//
+
+import Foundation
+
+class WeatherManager {
+    
+}
