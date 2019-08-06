ea9c005034076c1b7547864652ce3c10b37cc120
hngfu <djsl1018@naver.com>
2019-08-07 00:47:26 +0900

    DetailViewController 추가
    PageVC를 포함한 ContainerView를 가지고있음 PageControl, Button 추가 SmallDotPageControl 추가

---
+++ WeatherApp/WeatherApp/DetailWeatherViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DetailWeatherViewController.swift
+//  WeatherApp
+//
+//  Created by 조재흥 on 07/08/2019.
+//  Copyright © 2019 Hngfu. All rights reserved.
+//
+
+import UIKit
+
+class DetailWeatherViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
