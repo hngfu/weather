14c80b6ff7c1eabd49b4acd065a9389d3966b817
hngfu <djsl1018@naver.com>
2019-08-07 12:54:10 +0900

    앱 종료, 실행시 Preference 정보를 저장하고 불러오도록 함

--- WeatherApp/WeatherApp/AppDelegate.swift
+++ WeatherApp/WeatherApp/AppDelegate.swift
@@ -16,6 +16,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        if let data = UserDefaults.standard.value(forKey: Preference.key) as? Data,
+            let preference = try? PropertyListDecoder().decode(Preference.self, from: data) {
+            Preference.shared.replaceProperty(preference)
+        }
         return true
     }
 
@@ -27,6 +31,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        UserDefaults.standard.set(try? PropertyListEncoder().encode(Preference.shared),
+                                  forKey: Preference.key)
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
